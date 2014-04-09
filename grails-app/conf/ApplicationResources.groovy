ddbac3a9b071311f13c8db52b158c3c9d1ce8d79
yiguang hu <yighu@yahoo.com>
2014-04-08 22:49:27 -0400

    update resources
    update resources

--- grails-app/conf/ApplicationResources.groovy
+++ grails-app/conf/ApplicationResources.groovy
@@ -2,4 +2,31 @@ modules = {
     application {
         resource url:'js/application.js'
     }
+    menu {
+	dependsOn 'jquery'
+        resource url:'jm/menu.js'
+        resource url:'jm/menu.css'
+
+resource url:'jm/images/lavalamp-left.gif'
+resource url:'jm/images/lavalamp-left.png'
+resource url:'jm/images/lavalamp-right.gif'
+resource url:'jm/images/lavalamp-right.png'
+resource url:'jm/images/left.gif'
+resource url:'jm/images/left.png'
+resource url:'jm/images/right.gif'
+resource url:'jm/images/right.png'
+resource url:'jm/images/selected-left-sub.gif'
+resource url:'jm/images/selected-left-sub.png'
+resource url:'jm/images/selected-right-sub.gif'
+resource url:'jm/images/selected-right-sub.png'
+resource url:'jm/images/submenu-bottom.gif'
+resource url:'jm/images/submenu-bottom.png'
+resource url:'jm/images/submenu-pointer-bottom.gif'
+resource url:'jm/images/submenu-pointer-selected-bottom.png'
+resource url:'jm/images/submenu-pointer-top.png'
+resource url:'jm/images/submenu-selected-bottom.png'
+resource url:'jm/images/submenu-selected-top.png'
+resource url:'jm/images/subsubmenu-top.gif'
+resource url:'jm/images/subsubmenu-top.png'
+	}
 }
